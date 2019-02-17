dd8d5b8933e1dde638483209cd570718c6501164
Michal Muszynski <m.muszy@gmail.com>
2019-02-17 16:13:59 +0100

    Authorized paths

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { Switch, Route, Redirect } from "react-router-dom"
+import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom"
+import { withRouter } from "react-router"
 import App from "../views/entrypoint/Main"
 import HomePage from "../views/home/home-page"
 import LandingPage from "../views/landing-page/landing-page"
@@ -9,74 +10,49 @@ import AboutPage from "../views/landing-page/about-page/about-page"
 import { inject, observer } from "mobx-react"
 import User from "../stores/user-store"
 
-interface RoutesInterface {
+interface RoutesInterface extends RouteComponentProps<any> {
   userStore?: User
 }
 
 interface RoutesState {
-  loggedIn: boolean,
+  loggedIn: boolean
 }
-
-// const NonAuthRoute = ({ user: auth, component: Component, pathname: path, ...rest }) => (
-//   <Route {...rest} render={props => (
-//     !auth ? (
-//       <Component {...props} />
-//     ) : (
-//         <Redirect
-//           to={{ pathname: path }}
-//         />
-//       ))} />
-// );
 @inject("userStore")
 @observer
-class Routes extends React.Component<RoutesInterface, RoutesState, any> {
+class Routes extends React.Component<RoutesInterface, RoutesState> {
 
   state = {
     loggedIn: false
   }
 
-  authorizedRouting() {
+  authorizedRoutes = () => {
     return (
       <Switch>
         <Route path="/home" component={HomePage} />
+        <Redirect to="/home" />
       </Switch>
     )
   }
 
-  unauthorizedRouting() {
+  notAuthorizedRoutes = () => {
     return (
       <Switch>
         <Route path="/register" component={RegisterForm} />
         <Route path="/login" component={LoginForm} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/" component={LoginForm} />
-        {/* <Route path="*" component={PageNotFound} /> */}
+        <Route path="/" component={LoginForm} pathname={"/login"} />
       </Switch>
     )
   }
 
   render() {
-    // const PrivateRoute = ({ component: Component, ...rest } : { component: any, path: any }) => (
-    //   <Route {...rest} render={(props) => (
-    //     this.props.userStore.loggedIn ? 
-    //       <Component {...props} />
-    //       : 
-    //       <Redirect to='/login' />
-    //     )} 
-    //   />
-    // )
-
     return (
-      <Switch>
-        {
-          this.props.userStore.loggedIn ?
-           this.authorizedRouting()
-          :
-           this.unauthorizedRouting()
-        }
-      </Switch>
+      this.props.userStore.loggedIn ?
+        this.authorizedRoutes()
+      : 
+        this.notAuthorizedRoutes()
     )
   }
 }
 
-export default Routes
+export default withRouter<RoutesInterface>(Routes)
