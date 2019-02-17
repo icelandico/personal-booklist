b8e0f5e59c4d8e08d26309484361786d3c42466f
Michal Muszynski <m.muszy@gmail.com>
2019-02-17 20:05:30 +0100

    Working on API

--- src/api/app.js
+++ src/api/app.js
@@ -8,6 +8,7 @@ const passport = require("passport")
 const bcrypt = require("bcrypt")
 const saltRounds = 10
 const jwt = require("jwt-simple")
+const User = require("./signUp")
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -15,15 +16,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
-app.post("/api/register", (req, res) => {
-  const { email, password } = req.body
-  bcrypt.hash(password, saltRounds, (err,hash) => {
-    const INSERT_USER_QUERY = `
-    INSERT INTO "Users" (username, password) VALUES('${email}', '${hash}')
-  `
-    config.db.query(INSERT_USER_QUERY)
-  })
-})
+// app.post("/api/register", (req, res) => {
+//   const { email, password } = req.body
+//   bcrypt.hash(password, saltRounds, (err,hash) => {
+//     const INSERT_USER_QUERY = `
+//     INSERT INTO "Users" (username, password) VALUES('${email}', '${hash}')
+//   `
+//     //config.db.query(INSERT_USER_QUERY)
+//     console.log(hash)
+//   })
+// })
+
+app.post("/api/register", User.createUser)
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
--- src/api/signUp.js
+++ src/api/signUp.js
@@ -1,14 +1,31 @@
 const db = require("./config")
-const pgp = require('pg-promise')(initOptions)
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+// const pgp = require('pg-promise')(initOptions)
 
-const createUser = (username, password) => {
-  const query = `
-  INSERT INTO "Users"
-  (username, password)
-  VALUES($1 $2)
-  RETURNING *
-  `
-  return db.one(query, [username, password])
+
+// const createUser = (username, password) => {
+//   const query = `
+//   INSERT INTO "Users"
+//   (username, password)
+//   VALUES($1 $2)
+//   RETURNING *
+//   `
+//   return db.one(query, [username, password])
+// }
+
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) =>
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      return err ? reject(err) : resolve(hash)
+    })
+  )
+}
+
+const createUser = (request, response) => {
+  const user = request.body
+  hashPassword(user.password)
+    .then(pass => console.log(pass))
 }
 
 module.exports = { createUser }
