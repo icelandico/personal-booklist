99c928eac280e991cade95943af2dc25993886b4
Michal Muszynski <m.muszy@gmail.com>
2019-02-17 20:36:23 +0100

    Working on API

--- src/api/signUp.js
+++ src/api/signUp.js
@@ -1,6 +1,8 @@
-const db = require("./config")
+const config = require("./config")
 const bcrypt = require("bcrypt")
 const saltRounds = 10
+const jwt = require("jwt-simple")
+
 // const pgp = require('pg-promise')(initOptions)
 
 
@@ -22,10 +24,18 @@ const hashPassword = (password) => {
   )
 }
 
+const checkIfRegistered = email => {
+  const query = `SELECT id FROM "Users" WHERE username=$1`
+  const response = config.db.query(query, [email])
+  return response
+}
+
 const createUser = (request, response) => {
   const user = request.body
-  hashPassword(user.password)
-    .then(pass => console.log(pass))
+  checkIfRegistered(user.email)
+    .then(res => res.rowCount > 0 ? console.log("Already exists") : console.log("NO"))
+  // hashPassword(user.password)
+  //   .then(pass => console.log(pass))
 }
 
 module.exports = { createUser }
