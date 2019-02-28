7f0ac5ac5baa34d03eb6d284bbc97c5291fb065b
Michal Muszynski <m.muszy@gmail.com>
2019-02-28 18:57:37 +0100

    Copy logic

--- src/api/signIn.js
+++ src/api/signIn.js
@@ -1,21 +1,61 @@
-const db = require("./db_config")
+const config = require("./config")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+const jwt = require("jwt-simple")
 
-const findUserById = id => {
-  const query = `
-  SELECT *
-  FROM "Users"
-  WHERE id=$1
-  `
-  return db.oneOrNone(query, [id])
+const createUser = (user) => {
+  const newUser = {
+    username: user.email,
+    password: "",
+    token: ""
+  }
+  hashPassword(user.password)
+    .then(hashedPass => {
+      newUser.password = hashedPass
+    })
+    .then(() => createToken(newUser.username))
+    .then(token =>
+      newUser.token = token
+    )
+    .then(() => {
+      const insertUserQuery = `
+        INSERT INTO "users"
+        (username, password, token)
+        VALUES('${newUser.username}', '${newUser.password}', '${newUser.token}')
+        `
+      config.db.query(insertUserQuery)
+    }
+    )
 }
 
-const verifyUser = username => {
-  const query = `
-  SELECT *
-  FROM "Users"
-  WHERE username=$1
-  `
-  return db.oneOrNone(query, [username])
+const createToken = (username) => {
+  const timestamp = new Date().getTime()
+  const token = jwt.encode({ sub: username, iat: timestamp }, config.secret)
+  return token
 }
 
-module.exports = { findUserById, verifyUser }
+const hashPassword = async (password) => {
+  return new Promise((resolve, reject) =>
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      return err ? reject(err) : resolve(hash)
+    })
+  )
+}
+
+const checkIfRegistered = async email => {
+  const query = `SELECT id FROM "users" WHERE username=$1`
+  const response = await config.db.query(query, [email])
+  const exists = response.rowCount > 0
+  return exists
+}
+
+const registerProcedure = async (request, response) => {
+  const user = request.body
+  const alreadyRegistered = await checkIfRegistered(user.email)
+  response.send({ userExists: alreadyRegistered })
+  if (!alreadyRegistered) {
+    createUser(user)
+  }
+}
+
+module.exports = { registerProcedure }
