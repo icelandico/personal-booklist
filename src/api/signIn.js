75d0bd83894c676a61532a993a6a09c6b3c60ee8
Michal Muszynski <m.muszy@gmail.com>
2019-03-01 23:57:26 +0100

    Cleanup

--- src/api/signIn.js
+++ src/api/signIn.js
@@ -7,13 +7,7 @@ const auth = require("./passport")
 const loginProcedure = async (request, response) => {
   const { login, password } = request.body
   const res = await (auth.auth(login))
-  console.log(res)
-  // const user = request.body
-  // const alreadyRegistered = await checkIfRegistered(user.email)
-  // response.send({ userExists: alreadyRegistered })
-  // if (!alreadyRegistered) {
-  //   createUser(user)
-  // }
+
 }
 
 module.exports = { loginProcedure }
