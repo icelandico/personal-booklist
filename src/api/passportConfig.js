8311aba2fe96b9c3b31d076288bbf2f282736ddf
Michal Muszynski <m.muszy@gmail.com>
2019-03-06 18:57:25 +0100

    Passport local config

--- src/api/passportConfig.js
+++ src/api/passportConfig.js
@@ -12,23 +12,28 @@ const loginQuery = `
   WHERE username=$1 OR email=$1
   `
 
-const passportLogin = passport.use("login",
-  new LocalStrategy((login, password, done) => {
+const passportLogin = (passport) => 
+  passport.use("login", new LocalStrategy((login, password, done) => {
+    // Match user 
   config.db.query(loginQuery, [login], (err, result) => {
     if (err) {
       return done('Error with username', err)
     }
     if (result.rows.length > 0) {
       const user = result.rows[0]
+      // Match password
       bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+          throw err
+        }
         if (res) {
           done(null, { id: user.id, username: user.username, email: user.email })
         } else {
-          done(null, false)
+            done(null, false, { message: "Password is incorrect" })
         }
       })
     } else {
-      done(null, false)
+        done(null, false, { message: "User is not registered" })
     }
   })
 }))
@@ -38,14 +43,9 @@ passport.serializeUser((user, done) => {
   done(null, user.id)
 })
 
-passport.deserializeUser((id, cb) => {
-  db.query('SELECT id, username, type FROM users WHERE id = $1', [parseInt(id, 10)], (err, results) => {
-    if (err) {
-      winston.error('Error when selecting user on session deserialize', err)
-      return cb(err)
-    }
-
-    cb(null, results.rows[0])
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user)
   })
 })
 
